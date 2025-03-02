e78dd54e52c64604549ea1f2e947c36b907aea0d
tihaia <145658291+tihaia@users.noreply.github.com>
2025-03-02 23:52:29 +0200

    Add files via upload

---
+++ Lab2_react/00_initial/my-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Article from './components/Article'
+import ArticleList from './components/ArticleList'
+
+export default function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <ArticleList />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
