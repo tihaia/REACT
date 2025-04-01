114e7fae07a9621c1145fc67b67c993791627e28
tihaia <145658291+tihaia@users.noreply.github.com>
2025-04-02 02:23:57 +0300

    Update Header.jsx

--- lab3/my-app/src/components/Header.jsx
+++ lab3/my-app/src/components/Header.jsx
@@ -1,22 +1,24 @@
 import "../styles/Header.css";
+import Search from "./Search";
 
 /**
- * Компонент Header отображает шапку сайта с названием кофейни и навигационными ссылками.
- * Включает заголовок и ссылки для перехода к различным разделам страницы.
+ * Компонент Header отображает шапку сайта с поиском и навигацией.
  *
- * @returns {JSX.Element} Шапка с названием и меню навигации.
+ * @param {Function} onSearch Функция для обработки поиска.
+ * @returns {JSX.Element} Шапка с названием, поиском и меню навигации.
  */
-function Header() {
-    return (
-      <header className="header">
-        <h1 className="title">Tanya's Coffee</h1>
-        <nav className="nav">
-          <a href="#" className="link">Главная</a>
-          <a href="#menu" className="link">Меню</a>
-          <a href="#contacts" className="link">Найти нас</a>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header;
+function Header({ onSearch }) {
+  return (
+    <header className="header">
+      <h1 className="title">Tanya's Coffee</h1>
+      <nav className="nav">
+        <a href="#" className="link">Главная</a>
+        <a href="#menu" className="link">Меню</a>
+        <a href="#contacts" className="link">Найти нас</a>
+        <Search onSearch={onSearch} />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
