dbb173e7df095b413178915c93490dfb2800cbc2
tihaia <145658291+tihaia@users.noreply.github.com>
2025-04-02 02:24:26 +0300

    Update CoffeeList.jsx

--- lab3/my-app/src/components/CoffeeList.jsx
+++ lab3/my-app/src/components/CoffeeList.jsx
@@ -1,44 +1,31 @@
 import { useState, useEffect } from "react";
 import coffeeData from "../data/coffee.json";
 import CoffeeCard from "./CoffeeCard";
-import Search from "./Search";
 import "../styles/CoffeeList.css";
 
 /**
  * Компонент отображает список кофейных напитков с фильтрацией по названию.
- * Список обновляется при вводе текста в поле поиска.
  *
+ * @param {string} searchQuery - Текст для поиска.
  * @returns {JSX.Element} Список карточек кофейных напитков.
  */
-function CoffeeList() {
+function CoffeeList({ searchQuery }) {
   const [coffees, setCoffees] = useState([]);
-  const [filteredCoffees, setFilteredCoffees] = useState([]);
 
   useEffect(() => {
     setCoffees(coffeeData);
-    setFilteredCoffees(coffeeData);
   }, []);
 
-    /**
-   * Функция фильтрует напитки по запросу.
-   * @param {string} query - Текст для поиска.
-   */
-  const handleSearch = (query) => {
-    const lower = query.toLowerCase();
-    const filtered = coffees.filter((coffee) =>
-      coffee.name.toLowerCase().includes(lower)
-    );
-    setFilteredCoffees(filtered);
-  };
+  // Фильтрация списка кофе
+  const filteredCoffees = coffees.filter((coffee) =>
+    coffee.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
-    <div>
-      <Search onSearch={handleSearch} />
-      <div className="coffee-list">
-        {filteredCoffees.map((coffee) => (
-          <CoffeeCard key={coffee.id} coffee={coffee} />
-        ))}
-      </div>
+    <div className="coffee-list">
+      {filteredCoffees.map((coffee) => (
+        <CoffeeCard key={coffee.id} coffee={coffee} />
+      ))}
     </div>
   );
 }
